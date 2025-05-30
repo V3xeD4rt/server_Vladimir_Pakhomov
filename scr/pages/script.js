0b7aa5354a741ab833f036dc17d287b33589f491
Студент 0 2 <student_02@sakhgu.net>
2025-05-31 10:52:28 +1100

    Обнова script

---
+++ scr/pages/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const header = document.getElementById('header')
+
+    const info = {
+        SN: 'Пахомов Владимир',
+        group: '№231'
+    }
+
+    const infoElement = document.createElement('h1')
+    infoElement.textContent = `${info.Sn},${info.group}`
+    header.appendChild(infoElement)
+})
