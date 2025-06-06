30095d7845e9857f305db800786e1a4b23559402
Студент 0 2 <student_02@sakhgu.net>
2025-06-07 10:31:24 +1100

    Обнова 57

--- scr/pages/TodoList/resscr.js
+++ scr/pages/TodoList/resscr.js
@@ -1,15 +1,9 @@
 class ToDo{
     static loadDom(){
         document.addEventListener('DOMContentLoaded',() => {
-
-//            let i= 1;
-//            while (localStorage.getItem(`tasks${i}`));{
             const listTask = JSON.parse(localStorage.getItem('tasks'))
             for (let i = 0;i<listTask.length; i++)
             ToDo.createElement(listTask[i])
-                
-//            }
-//            const listTask = JSON.parse(localStorage.getItem('tasks')) 
         })
     }
     static createElement(text){
@@ -24,21 +18,8 @@ class ToDo{
         const button = document.getElementById('clear')
         button.addEventListener('click',() => {
             const task = document.getElementById('dig1').value
-/*            if (task==="")return
-            let i = 1
-            let a = localStorage.getItem("taskIndex")
-            if(!a) a=0
-            a++
-
-            //while (localStorage.getItem(`tasks${i}`)); i++
-
-            localStorage.setItem("taskIndex",a)
-*/
-//            localStorage.setItem(`task${a}`,task)
             ToDo.createElement(task)
 
-
-            
             let arr = []
             if (localStorage.getItem('tasks'))
             arr = JSON.parse(localStorage.getItem('tasks'))
--- scr/pages/web_dev/resscr.js
+++ scr/pages/web_dev/resscr.js
@@ -1,3 +1,32 @@
+class Calc{
+    constructor(){
+        const dig1 = document.getElementById("dig1")
+        const dig2 = document.getElementById("dig2")
+        let res = document.getElementById("res")
+        document.getElementsByName("plus")[0].onclick = function(){
+            res.value = Number(dig1.value) + Number(dig2.value)
+        };
+        document.getElementsByName("minus")[0].onclick = function(){
+            res.value = Number(dig1.value) - Number(dig2.value)
+        };
+        document.getElementsByName("delen")[0].onclick = function(){
+            if (dig1.value == 0 || dig2.value == 0) {res.value="Делить на 0 нельзя"}
+            else {res.value = Number(dig1.value) / Number(dig2.value)}
+        };
+        document.getElementsByName("umnoj")[0].onclick = function(){
+            res.value = Number(dig1.value) * Number(dig2.value)
+        };
+        document.getElementsByName("clear")[0].onclick = function(){
+            dig1.value =""
+            dig2.value =""
+            res.value =""
+        };
+    }
+}
+
+
+
+/*
 const dig1 = document.getElementById("dig1")
 const dig2 = document.getElementById("dig2")
 let res = document.getElementById("res")
@@ -19,3 +48,4 @@ document.getElementsByName("clear")[0].onclick = function(){
     dig2.value =""
     res.value =""
 };
+*/
